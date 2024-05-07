cf2a42620cb39ccfeb03dc517e45aa999c93b601
Kintoki Sensei <weehow_wong@outlook.com>
2024-05-07 22:39:22 +0800

    Fixed pagination blogpost content rendering

--- public/scripts/pagination.js
+++ public/scripts/pagination.js
@@ -11,13 +11,12 @@ window.addEventListener('scroll', async () => {
   const clientHeight = document.documentElement.clientHeight;
 
   if (scrollTop + clientHeight === scrollHeight) {
-    console.log(pageCounter, totalPageCount)
     if (pageCounter < totalPageCount) {
-      console.log('hello world')
       try {
         const blogs = await fetch(`/page/${pageCounter}`);
         const { moreBlogs } = await blogs.json();
         moreBlogs.forEach(blog => {
+          // console.log(blog)
           blogpostContainer.appendChild(populate(blog));
         })
 
@@ -27,36 +26,42 @@ window.addEventListener('scroll', async () => {
         console.log(error.message);
       }
     }
-    console.log('still working')
   }
 });
 
 function populate(blog) {
-  
+
   // blogpost container
   const blogpost = document.createElement('div');
   blogpost.classList.add('blogpost');
   
   // blogpost title
-  const titleText = parser.parseFromString(blog.title, 'text/html').body.firstChild.textContent;
   const title = document.createElement('h1');
-  title.innerHTML = titleText;
+  title.innerHTML = blog.title;
   blogpost.appendChild(title);
 
   // blogpost content
-  const content = parser.parseFromString(blog.sanitizedHtml, 'text/html').body;
-  const n = content.children.length
-  for (let i = 0; i < n; i++) {
-    const paragraph = document.createElement(content.children[i].localName);
-    paragraph.innerHTML = content.children[i].textContent;
-    blogpost.appendChild(paragraph);
+  const htmlList = blog.sanitizedHtml.split('\n');
+  for (let i = 0; i < htmlList.length - 1; i++) {
+    blogpost.insertAdjacentHTML('beforeend', htmlList[i])
   }
 
   // blogpost dateCreated
-  const dateCreated = parser.parseFromString(blog.dateCreated, 'text/html').body.firstChild.textContent;
+  const dateCreated = blog.dateCreated;
   const date = document.createElement('h6');
   date.innerHTML = dateCreated;
   blogpost.appendChild(date);
+
+  // share-links
+  const links = document.createElement('div');
+  links.classList.add('share-lin');
+  links.innerHTML = `
+    <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+    <a href="#"><i class="fa-brands fa-square-x-twitter"></i></a>
+    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+    <a href="#"><i class="fa-solid fa-link"></i></a>
+  `;
+  blogpost.appendChild(links);
   
   return blogpost;
 }
